97077496a3414780831e92a025c3bc1d626ddd9e
Pranav Parwate <40783010+Twon99@users.noreply.github.com>
2024-06-10 19:52:11 -0700

    Time: 85 ms (19.62%), Space: 16.6 MB (88.86%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
@@ -9,7 +9,7 @@ class Solution:
         dq = deque()
         dq.append(s[i])
         lon = 1
-        while(i<n and j<n):
+        while(j<n):
             if s[j] not in dq:
                 dq.append(s[j])
                 lon = max(lon, len(dq))
