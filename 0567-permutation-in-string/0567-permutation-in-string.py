24392c8f2ac34767821cbd8e7592a6ee5bef7f58
Pranav Parwate <40783010+Twon99@users.noreply.github.com>
2024-06-11 22:06:31 -0700

    Time: 61 ms (59.03%), Space: 16.6 MB (67.66%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.py
+++ 0567-permutation-in-string/0567-permutation-in-string.py
@@ -1,34 +1,58 @@
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
-        if len(s1) > len(s2):
-            return False
-
-        s1Count, s2Count = [0] * 26, [0] * 26
-        for i in range(len(s1)):
-            s1Count[ord(s1[i]) - ord("a")] += 1
-            s2Count[ord(s2[i]) - ord("a")] += 1
-
-        matches = 0
-        for i in range(26):
-            matches += 1 if s1Count[i] == s2Count[i] else 0
-
-        l = 0
-        for r in range(len(s1), len(s2)):
-            if matches == 26:
+        
+        frequency_dict = {}
+    
+        # Loop through each character in the string
+        for char in s1:
+            # If the character is already in the dictionary, increment its count
+            if char in frequency_dict:
+                frequency_dict[char] += 1
+        # If the character is not in the dictionary, add it with a count of 1
+            else:
+                frequency_dict[char] = 1
+        print(frequency_dict)
+        i = 0 
+        j = len(s1) - 1
+        window_frequency = {}
+        #dq = deque()
+        #dq.append(s2[i])
+        #while(j < len(s1)):
+            #dq.append(s2[j])
+        for char in s2[i:j+1]:
+            if char in window_frequency:
+                window_frequency[char] += 1
+            else:
+                window_frequency[char] = 1
+        #j = len(s1)
+        #print(window_frequency)
+        while( j < len(s2) and i< len(s2)):
+            
+            if window_frequency == frequency_dict:
+                print("window",window_frequency )
+                print("frequency_dict", frequency_dict)
+                
                 return True
+                
+            else:
+                #print("i = ", str(i) + "s[i] = ", str(s2[i]))
+                #print(window_frequency)
+                if s2[i] in window_frequency:
+                    window_frequency[s2[i]] -= 1
 
-            index = ord(s2[r]) - ord("a")
-            s2Count[index] += 1
-            if s1Count[index] == s2Count[index]:
-                matches += 1
-            elif s1Count[index] + 1 == s2Count[index]:
-                matches -= 1
-
-            index = ord(s2[l]) - ord("a")
-            s2Count[index] -= 1
-            if s1Count[index] == s2Count[index]:
-                matches += 1
-            elif s1Count[index] - 1 == s2Count[index]:
-                matches -= 1
-            l += 1
-        return matches == 26
+                    
+                if window_frequency[s2[i]] == 0:
+                    del window_frequency[s2[i]]
+                i = i + 1
+                j = j + 1
+                if j >= len(s2):
+                    break
+                if s2[j] in window_frequency:
+                    window_frequency[s2[j]] += 1
+                else:
+                    window_frequency[s2[j]] = 1
+                #print("j = ", str(j) + "s[j] = ", str(s2[j]))
+                #print(window_frequency)
+               
+        return False
+                
