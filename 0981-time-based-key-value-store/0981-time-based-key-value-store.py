b01f62be58d7426bff5e36970d9b1e8622d6775c
Pranav Parwate <40783010+Twon99@users.noreply.github.com>
2024-06-19 20:16:06 -0700

    Time: 715 ms (5.02%), Space: 74.2 MB (37.78%) - LeetHub

--- 0981-time-based-key-value-store/0981-time-based-key-value-store.py
+++ 0981-time-based-key-value-store/0981-time-based-key-value-store.py
@@ -1,29 +1,36 @@
 class TimeMap:
 
     def __init__(self):
-        self.keyStore = {}  # key : list of [val, timestamp]
+        self.mapof_Map = {}
+        
 
     def set(self, key: str, value: str, timestamp: int) -> None:
-        if key not in self.keyStore:
-            self.keyStore[key] = []
-        self.keyStore[key].append([value, timestamp])
 
-
+        if key not in self.mapof_Map:
+            self.mapof_Map[key] = [[value, timestamp]]
+        else:
+            self.mapof_Map[key].append([value, timestamp])
         
+
 
     def get(self, key: str, timestamp: int) -> str:
-        res, values = "", self.keyStore.get(key, [])
-        l, r = 0, len(values) - 1
-        while l<=r:
-            m = (l + r) // 2
+        target = timestamp
+        if key not in self.mapof_Map:
+            return ""
+        else:
+            l, r = 0, len(self.mapof_Map[key]) - 1
+            res = ''
+            mid = 0
+            while(l <= r):
+                mid = int((l+r)//2)
+                if self.mapof_Map[key][mid][-1] > target:
+                    r = mid - 1
+                elif self.mapof_Map[key][mid][-1] <= target:
+                    res = self.mapof_Map[key][mid][0]
+                    l = mid + 1
+                
+            return res
 
-            if values[m][1] <= timestamp:
-                res = values[m][0]
-                l = m + 1
-            else:
-                r = m - 1
-        return res
-        
         
 
 
