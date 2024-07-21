1be75794606cb742d11aa6d76277187682a484ea
Pranav Parwate <40783010+Twon99@users.noreply.github.com>
2024-07-21 12:29:28 -0700

    Time: 519 ms (47.20%), Space: 42.5 MB (25.91%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.py
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.py
@@ -2,15 +2,30 @@ class Solution:
     def longestCommonSubsequence(self, text1: str, text2: str) -> int:
         n = len(text1)
         m = len(text2)
-        dp = [[-1 for j in range(m)] for i in range(n)]
-        def help( ind1, ind2 , dp):
-            if ind1 < 0 or ind2 < 0:
-                return 0
-            if dp[ind1][ind2] != -1:
-                return dp[ind1][ind2]
-            if text1[ind1] == text2[ind2]:
-                dp[ind1][ind2] = 1 + help(ind1 - 1, ind2 - 1 , dp)
-            else:
-                dp[ind1][ind2] = 0 + max(help(ind1 , ind2 -1, dp), help(ind1 - 1, ind2, dp))
-            return dp[ind1][ind2]
-        return help(len(text1) - 1 , len(text2) - 1 , dp)
+        dp = [[-1 for j in range(m + 1)] for i in range(n + 1)]
+        for i in range(n + 1):
+            dp[i][0] = 0
+        for j in range(m + 1):
+            dp[0][j] = 0
+        for ind1 in range(1 , n + 1):
+            for ind2 in range(1 , m + 1):
+                if text1[ind1 - 1] == text2[ind2 - 1]:
+                    dp[ind1][ind2] = 1 + dp[ind1 - 1][ind2 - 1]
+                else:
+                    dp[ind1][ind2] = 0 + max(dp[ind1 -1][ind2], dp[ind1][ind2 - 1])
+        return dp[n][m]
+
+
+
+        # def help( ind1, ind2 , dp):
+        #     if ind1 < 0 or ind2 < 0:
+        #         return 0
+        #     if dp[ind1][ind2] != -1:
+        #         return dp[ind1][ind2]
+        #     if text1[ind1] == text2[ind2]:
+        #         dp[ind1][ind2] = 1 + help(ind1 - 1, ind2 - 1 , dp)
+        #     else:
+        #         dp[ind1][ind2] = 0 + max(help(ind1 , ind2 -1, dp), help(ind1 - 1, ind2, dp))
+        #     return dp[ind1][ind2]
+        # return help(len(text1) - 1 , len(text2) - 1 , dp)
+
