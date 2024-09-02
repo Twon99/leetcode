f786e742658a1df5a415b0e877bba656ef46260b
Pranav Parwate <40783010+Twon99@users.noreply.github.com>
2024-09-02 11:55:50 -0700

    Time: 313 ms (9.69%), Space: 19.2 MB (5.69%) - LeetHub

--- 0455-assign-cookies/0455-assign-cookies.py
+++ 0455-assign-cookies/0455-assign-cookies.py
@@ -1,6 +1,5 @@
 class Solution:
     def findContentChildren(self, g: List[int], s: List[int]) -> int:
-
         g.sort()
         s.sort()
         print(s)
@@ -17,6 +16,5 @@ class Solution:
                 i += 1
             
         return res
-
 
         
