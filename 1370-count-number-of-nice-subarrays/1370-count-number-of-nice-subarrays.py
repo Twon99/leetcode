300079e77b4ef7e7955903a3a78c62006295e4f8
Pranav Parwate <40783010+Twon99@users.noreply.github.com>
2024-10-04 20:14:02 -0700

    Time: 612 ms (53.85%), Space: 23.7 MB (42.52%) - LeetHub

--- 1370-count-number-of-nice-subarrays/1370-count-number-of-nice-subarrays.py
+++ 1370-count-number-of-nice-subarrays/1370-count-number-of-nice-subarrays.py
@@ -1,29 +1,22 @@
 class Solution:
     def numberOfSubarrays(self, nums: List[int], k: int) -> int:
 
-        for i in range(len(nums)):
-            if nums[i] % 2 == 0:
-                nums[i] = 0
-            else:
-                nums[i] = 1
-        print(nums)
-        def atMost(S):
-            """Return number of subarrays with sum at most S."""
-            if S < 0:
-                return 0
-            curr_sum = 0
-            left = 0
-            count = 0
+
+        l = 0
+        m = 0
+        odd = 0
+        res = 0
+        for r in range(len(nums)):
+            if nums[r]%2 == 1:
+                odd += 1
+            while odd > k:
+                if nums[l]%2 == 1:
+                    odd -=1
+                l += 1
+                m = l
+            if odd == k:
+                while not nums[m]%2 == 1:
+                    m += 1
+                res += m - l + 1
+        return res
             
-            for right in range(len(nums)):
-                curr_sum += nums[right]
-                
-                while curr_sum > S:
-                    curr_sum -= nums[left]
-                    left += 1
-                
-                # Add the number of subarrays that end at 'right'
-                count += right - left + 1
-            
-            return count
-        return atMost(k) - atMost(k - 1)
